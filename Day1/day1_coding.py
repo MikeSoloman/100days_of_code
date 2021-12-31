bb96dadee508efc2f487d3c5f2035a388dbd96b9
MikeSoloman <sqasoloman@gmail.com>
2021-12-31 15:43:51 -0500

    Day 1 is Done

---
+++ Day1/day1_coding.py
@@ -0,0 +1,9 @@
+#Basic python inputing, commenting, printing
+#Random Band generator
+
+print("Welcome to Mike's Random Band Creator\n"
+      "let's get your random band name generated...")
+city_name = input("What's the city that you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+
+print(f"Your random band name is {pet_name} {city_name}")
